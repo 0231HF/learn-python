99796fe002b0f66060f8b8f5c40ce2b71e4c9bd8
0231HF <1755543987@qq.com>
2024-01-31 15:07:20 +0800

    01-31 add 新增变量练习题目内容

---
+++ python基础/code/变量相关练习题目.py
@@ -0,0 +1,77 @@
+'''
+1. 看代码结果
+
+   ```python
+   alex_length = 3
+   wupeiqi_length = 18
+   total = alex_length + wupeiqi_length
+   print(total)
+   ```
+
+2. 按要求写代码
+
+   ```python
+   # 假设age为小明的年龄
+   age = 18
+   # 问：已知小李比小明大3岁，计算小李年龄赋值给ming_age变量并输出。
+   ming_age = age + 3
+   print(ming_age)
+   # 问：已知大刘比小明和小李年龄的和还要大5岁，计算大刘的年龄赋值给liu_age变量并输入。
+   liu_age = age + ming_age + 5
+   print(liu_age)
+   ```
+
+3. 看代码写结果
+
+   ```python
+   nickname = "一米八大高个"
+   username = nickname
+
+   username = "弟弟"
+   print(nickname)
+   print(username)
+   ```
+
+4. 看代码写结果
+
+   ```python
+   nickname = "一米八大高个"
+   username = nickname
+   nickname = "弟弟"
+
+   print(nickname)
+   print(username)
+   ```
+
+5. 看代码写结果
+
+   ```python
+   nickname = "一米八大高个"
+   username = "弟弟"
+   text = username + nickname
+   print(text)
+   ```
+
+6. 看代码写结果
+
+   ```python
+   nickname = "一米八大高个"
+   username = nickname
+   nickname = "弟弟"
+   text = username + nickname
+   print(text)
+   ```
+
+7. 看代码写结果
+
+   ```python
+   string_number = "20"
+   num = int(string_number)
+
+   data = string_number * 3
+   print(data) # "202020"
+
+   value = num * 3
+   print(value) # 60
+   ```
+'''
