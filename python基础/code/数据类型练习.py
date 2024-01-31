b6b3be86e6e64e2fd76e882a53cd859e8c0ee87b
0231HF <1755543987@qq.com>
2024-01-31 11:03:18 +0800

    01-31 fix 修改数据类型练习题目

--- python基础/code/数据类型练习.py
+++ python基础/code/数据类型练习.py
@@ -33,3 +33,26 @@
 
 注意：类型转换不是改变原来值，实际在底层是新创建了一个值。例如有整数 6 ，然后使用 str(6) 转化了一下得到 “6”,实际上这个字符串”6”是依据整数6新创建的。
 """
+
+print(50 * 10 / 5)
+print(8 > 10)
+print(30 % 2)
+print("我爱我的祖国" * 3)
+print("xxx" == "yyy")
+print(666 == 666)
+print(666 == "666")
+
+print("xs------")
+print(int("100") * 3)  # 300
+print(int("123") + int("88"))  # 211
+print(str(111) + str(222))  # 111222
+print(str(111) * 3)  # 111111111
+print(int("8") > 7)  # True
+print(str(111) == 111)  # False
+print(bool(-1))  # True
+print(bool(0))  # False
+print(bool(""))  # Falseß
+print(bool("你好"))  # True
+print(True == True)  # True
+print(True == False)  # False
+print(bool("") == bool(0))  # True
