9f224b84d7312d08ef628b1370891e02916f4c34
0231HF <1755543987@qq.com>
2024-02-27 18:26:01 +0800

    0227-add-添加面向对象特性相关知识点

---
+++ python面向对象/code/实现用户注册和登录.py
@@ -0,0 +1,36 @@
+class User:
+    def __init__(self, name, pwd):
+        self.name = name
+        self.pwd = pwd
+
+
+class Account:
+    def __init__(self):
+        # 用户列表，数据格式：[user对象，user对象，user对象]
+        self.user_list = []
+
+    def login(self):
+        """
+        用户登录，输入用户名和密码然后去self.user_list中校验用户合法性
+        :return:
+        """
+        pass
+
+    def register(self):
+        """
+        用户注册，没注册一个用户就创建一个user对象，然后添加到self.user_list中，表示注册成功。
+        :return:
+        """
+        pass
+
+    def run(self):
+        """
+        主程序
+        :return:
+        """
+        pass
+
+
+if __name__ == '__main__':
+    obj = Account()
+    obj.run()
--- python面向对象/code/面向对象之cs小游戏.py
+++ python面向对象/code/面向对象之cs小游戏.py
@@ -11,3 +11,61 @@
 他们都可以看自己的血量  都可以射击 每次射击 扣除对面5滴血 可以扫射 扫射 10点血
 
 """
+
+
+class People:
+    def __init__(self, name):
+        self.HP = 0
+        self.name = name
+
+    def show_hp(self):
+        print("{}的血量为{}".format(self.name, self.HP))
+
+    def shoot(self, people):
+        people.HP -= 5
+        people.show_hp()
+
+    def strafe(self, people_list):
+        for people in people_list:
+            people.HP -= 10
+            people.show_hp()
+            if people.HP <= 0:
+                print("{} is dead".format(people))
+            else:
+                pass
+
+
+class Police(People):
+    def __init__(self, name, role):
+        super().__init__(name)
+        self.name = name
+        if role == "队员":
+            self.HP = 200
+        else:
+            self.HP = 300
+
+    def bomb(self, bandits):
+        for bandit in bandits:
+            bandit.HP -= 200
+            bandit.show_hp()
+
+
+class Bandit(People):
+    def __init__(self, name):
+        super().__init__(name)
+        self.name = name
+        self.HP = 300
+
+
+if __name__ == "__main__":
+    p1 = Police("xxx", "队员")
+    p2 = Police("zzz", "队员")
+    p3 = Police("ccc", "队长")
+
+    b1 = Bandit("xsaxa")
+    b2 = Bandit("dasdasd")
+    print("".center(20, "*"))
+    p1.shoot(b2)
+    p1.bomb([b2])
+    print("{}炸了{} 、{}、 {}".format(b1.name, p1.name, p2.name, p3.name))
+    b2.strafe([p1, p2, p3])
